77394e86d61f28539d8286081b20a79650004b5f
Nuwan Rathnayake <me@nuwanr.com>
2023-06-27 01:47:04 +0530

    datatables server side search

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,12 +22,32 @@ class OrderController extends Controller
             'status',
             null
         ];
+
+        $columnsSearch = [
+            'id',
+            ['sql'=>'(SELECT SUM(amount) FROM order_items WHERE order_id = orders.id) like ?'],
+            ['sql' =>'CONCAT_WS(" ", TRIM(BOTH "\"" FROM JSON_EXTRACT(checkout_data, "$.first_name")), TRIM(BOTH "\"" FROM JSON_EXTRACT(checkout_data, "$.last_name"))) like ?'],
+            'payment_method',
+            'payment_status',
+            'status',
+        ];
+
+
         $ordersQuery = Order::query()
             ->selectRaw('orders.*,
             (SELECT SUM(amount) FROM order_items WHERE order_id = orders.id) AS total,
             CONCAT_WS(" ",TRIM(BOTH "\"" FROM JSON_EXTRACT(checkout_data, "$.first_name")),TRIM(BOTH "\"" FROM JSON_EXTRACT(checkout_data, "$.last_name")))  AS name
             ');
 
+        if($request->search['value']){
+            foreach($columnsSearch as $column){
+                if(is_array($column)){
+                    $ordersQuery->orWhereRaw($column['sql'], ["%{$request->search['value']}%"] );
+                }else{
+                    $ordersQuery->orWhere($column, 'like', ["%{$request->search['value']}%"] );
+                }
+            }
+        }
         if($request->order && !empty($columnsOrder[$request->order[0]['column']])){
             $ordersQuery->orderBy($columnsOrder[$request->order[0]['column']], $request->order[0]['dir'] );
         }else{
@@ -57,7 +77,7 @@ class OrderController extends Controller
         $output = [
             "draw" => $request->draw,
             "recordsTotal" => Order::count(),
-            "recordsFiltered" =>  Order::count(),
+            "recordsFiltered" =>  $ordersQuery->count(),
             "data" => $data,
         ];
 
